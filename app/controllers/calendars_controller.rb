42bd8fa92cad7756c9df4220dbf7bab689889d41
xingyoudeac <yukichishichuan@gmail.com>
2021-04-30 18:21:41 +0900

    Issue2とIssue3の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,7 +1,7 @@
 class CalendarsController < ApplicationController
 
   def index
-    getweek
+    get_week
     @plan = plan.new
   end
 
@@ -16,7 +16,7 @@ class CalendarsController < ApplicationController
     params.require(:calendars).permit(:date, :plan)
   end
 
-  def getweek
+  def get_week
 
     wdays = ['(日)','(月)','(火)','(水)','(木)','(金)','(土)']
 
